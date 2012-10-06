f4702c6ff7c1d5da59aa015af647ba92b5f777d8
Colin Harris <colin.harris@lambdasoftware.com.au>
2012-10-06 11:01:45 +1000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PizzaCrusts::Application.config.secret_token = '0f6120651150388f05af2b0ccd5b3f96d0ebc706db7d39c25b913a4765fc39b80cf1b3bfa453dc898eabe905f942ba418a10480ecc9b83a49bb29ff2582a0013'
